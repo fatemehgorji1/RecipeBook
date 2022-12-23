0d9ff4b584c787386d0d882a7957d0d3db5bb744
fatemehgorji <fatemehgorji77@gmail.com>
2022-12-23 17:57:33 +0330

    iii

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -27,17 +27,7 @@ export class AuthService {
       password: password,
       returnSecureToken: true
 
-    }).pipe(catchError(errorRes => {
-      let errorMessage = 'an error unknown occurred';
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'Email is already used';
-      }
-      return throwError(errorMessage);
-    }))
+    }).pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
@@ -45,26 +35,27 @@ export class AuthService {
       email: email,
       password: password,
       returnSecureToken: true
-    }).pipe(catchError(errorRes => {
-
-      let errorMessage = 'an error unknown occurred';
-
-      if (!errorRes.error || !errorRes.error.error) {
-        return throwError(errorMessage);
-      }
-      switch (errorRes.error.error.message) {
-
-        case 'INVALID_PASSWORD':
-          errorMessage = 'The password entered is not correct';
-          break;
-        case 'EMAIL_NOT_FOUND':
-          errorMessage = 'The email entered was not found';
-          break;
-      }
-      return throwError(errorMessage);
-    }))
+    }).pipe(catchError(this.handleError));
   }
 
+  private handleError(errorRes: HttpErrorResponse) {
 
+    let errorMessage = 'an error unknown occurred';
+
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+
+      case 'INVALID_PASSWORD':
+        errorMessage = 'The password entered is not correct';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'The email entered was not found';
+        break;
+    }
+    return throwError(errorMessage);
+
+  }
 }
 
