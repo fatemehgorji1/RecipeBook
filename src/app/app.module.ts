79f185970b8b1f89a32c450dc2a97e56260db6c6
fatemehgorji <fatemehgorji77@gmail.com>
2023-01-14 01:49:07 +0330

    kkk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from "@angular/common/http";
+
 //my new modules
 import { CoreModule } from 'src/app/core.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { AuthModule } from 'src/app/auth/auth.module';
 import { RecipesModule } from 'src/app/recipes/recipes.module';
 import { ShoppingListModule } from 'src/app/shopping-list/shopping-list.module';
-//
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from "@angular/common/http";
 
-//
+//components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from 'src/app/header/header.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
@@ -32,6 +32,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
   ],
   imports: [
+
     ReactiveFormsModule,
     FormsModule,
     BrowserModule,
